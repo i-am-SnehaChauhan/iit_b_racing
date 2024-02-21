4d53d56936833384e5e09f4fa92f3d9f6d46796a
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-02-21 16:42:48 +0530

    Hero Section Completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,42 @@
+"use client"
 import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+
 
 const Hero = () => {
+        const handleScroll = () => {
+            
+        }
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Welcome to the IIT-B Racing
+            </h1>
+            <p className="hero__subtitle">
+                We are a student team that designs, manufactures and tests formula-style
+            </p>
+
+            <CustomButton
+             title = "Read More"
+             containerStyle = "bg-primary-blue text-white rounded-full mt-10"
+             handleClick = {handleScroll} 
+             />
+ 
+        </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image
+                    src="/hero.png"
+                    alt="Hero-Image"
+                    fill className="object-contain"
+                />
+                <div className="hero__image-overlay"></div>
+                    
+            </div>
+        </div>
+    </div>
   )
 }
 
