6ed336dd517462111d47099cbd5bfa063c914f1c
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-02-21 22:35:37 +0530

    Hero Section Completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,26 +5,26 @@ import CustomButton from "./CustomButton";
 
 const Hero = () => {
   
-  const words2 = [
-    {
-      text: "IIT",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-    {
-      text:"Bombay",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-    {
-      text: "Racing.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
+  // const words2 = [
+  //   {
+  //     text: "IIT",
+  //     className: "text-blue-500 dark:text-blue-500",
+  //   },
+  //   {
+  //     text:"Bombay",
+  //     className: "text-blue-500 dark:text-blue-500",
+  //   },
+  //   {
+  //     text: "Racing.",
+  //     className: "text-blue-500 dark:text-blue-500",
+  //   },
+  // ];
   const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title text-[#00004D]">Welcome to the </h1>
-        <TypewriterEffectSmooth words={words2} />
+        <h1 className="hero__title text-[#00004D]">Welcome to the IIT Bombay Racing</h1>
+        {/* <TypewriterEffectSmooth words={words2} /> */}
         <p className="hero__subtitle">
           Join us in our journey to build the fastest electric car in India
         </p>
