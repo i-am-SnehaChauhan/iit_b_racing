02fed62509f42963172402a067a04aac0c8aad37
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-02-24 22:18:32 +0530

    Error resolved

--- components/TeamCard.tsx
+++ components/TeamCard.tsx
@@ -1,50 +1,65 @@
-"use client";
-
-import {teamData }from '../constants/teamData';
+import { teamData } from '../constants/teamData';
 import Image from 'next/image';
 
-const SocialMediaLinks = ({ socials }) => (
+interface SocialMediaLink {
+    url: string;
+    icon: string;
+}
+
+interface SocialMediaLinksProps {
+    socials: SocialMediaLink[];
+}
+
+interface CardProps {
+    name: string;
+    title: string;
+    socials: SocialMediaLink[];
+}
+
+interface CardContainerProps {
+    teamData: typeof teamData; 
+}
+
+const SocialMediaLinks = ({ socials }: SocialMediaLinksProps) => (
     <div className="social-media-links">
-      {socials.map((item, index) => (
-        <div key={index} className="social-media-link">
-          <a href={item.url} target="_blank" rel="noopener noreferrer">
-           <img src={item.icon}/>
-          </a>
-        </div>
-      ))}
+        {socials.map((item, index) => (
+            <div key={index} className="social-media-link">
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                    <img src={item.icon} />
+                </a>
+            </div>
+        ))}
     </div>
 );
 
-
-const Card = ({ image, name,title, socials }) => (
+const Card = ({ image, name, title, socials }: CardProps) => (
     <div className="card">
-      <div className="card-content">
-        <Image src={image} alt={name} className="avatar" />
-         <div className="info">
-          <h3 className="font-serif text-2xl">{name}</h3>
-          <h4 style={{color:"#ffffffef",fontSize:"15px"}}>{title}</h4>
-          <SocialMediaLinks socials={socials} />
+        <div className="card-content">
+            <Image src={image} alt={name} className="avatar" width={150} height={150} />
+            <div className="info">
+                <h3 className="font-serif text-2xl">{name}</h3>
+                <h4 style={{ color: "#ffffffef", fontSize: "15px" }}>{title}</h4>
+                <SocialMediaLinks socials={socials} />
+            </div>
         </div>
-      </div>
     </div>
 );
 
-const CardContainer = ({ teamData }) => (
+const CardContainer = ({ teamData }: CardContainerProps) => (
     <div className='floating'>
-    <div className="card-container">
-      {teamData.map((card, index) => (
-        <Card key={index} {...card} />
-      ))}
+        <div className="card-container">
+            {teamData.map((card, index) => (
+                <Card key={index} {...card} />
+            ))}
+        </div>
     </div>
-    </div>
-  );
+);
 
-  const TeamCard = () => (
+const TeamCard = () => (
     <div className="team">
-      <h1 className='heading'>Our Team</h1>
-      <CardContainer teamData={teamData} />
+        <h1 className='heading'>Our Team</h1>
+        <CardContainer teamData={teamData} />
     </div>
-  );
-  
-  export default TeamCard;
-  
+);
+
+export default TeamCard;
